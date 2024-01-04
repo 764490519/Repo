add8f13adf28972d23e2b5f391952f018b870e69
764490519 <liangguoqing777@gmail.com>
2024-01-04 16:28:30 +0100

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -1,10 +1,6 @@
 #include <stdio.h>;
 
-<<<<<<< HEAD
 int a = 1;
-=======
-int a = 0;
->>>>>>> 1e7fcd5d3ac88363fe2a7b1f5585af5379e2f725
 
 int main()
 {
@@ -12,15 +8,8 @@ int main()
     printf("Git Kraken");
     return 0;
 }
-<<<<<<< HEAD
 
 int add1(int a)
 {
     a++;
-=======
-int add2(int a)
-{
-    a++;
-    a++;
->>>>>>> 1e7fcd5d3ac88363fe2a7b1f5585af5379e2f725
 }
